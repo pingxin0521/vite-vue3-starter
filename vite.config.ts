ba035e9f472532deb8e53cab7abc49f1148e11d1
pingxin0521 <m138394441583@163.com>
2021-04-17 15:56:48 +0800

    fix: git

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,18 @@ export default defineConfig({
   },
   base: './', // 设置打包路径
   server: {
-    port: 4000,
-    open: true,
-    cors: true
+    port: 4500, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true // 允许跨域
+
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:x000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // },
   }
 })
