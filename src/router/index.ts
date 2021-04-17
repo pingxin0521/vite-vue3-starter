72bccfd80c3552bf9edced200bc8f77a002cffc1
pingxin0521 <m138394441583@163.com>
2021-04-17 15:59:23 +0800

    fix: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '@/views/home.vue'
-import Vuex from '@/views/vuex.vue'
+import Home from '@/views/Home.vue'
+import Vuex from '@/views/Vuex.vue'
 import Test from '@/views/Test.vue'
 
 const routes: RouteRecordRaw[] = [
@@ -17,7 +17,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/axios',
     name: 'Axios',
-    component: () => import('@/views/axios.vue') // 懒加载组件
+    component: () => import('@/views/Axios.vue') // 懒加载组件
   },
   {
     path: '/test',
